78656bc8a089d44c5c59166a60c7b79e71266bf3
Adrian-Koh <adriankoh216@yahoo.com>
2025-05-10 17:35:44 +0800

    Fix width/height of grid even if squares updated

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
 const container = document.querySelector('#container');
+const GRID_WIDTH = 900;
 
-
-const btn = document.createElement('button');
-btn.textContent = 'Update squares per side';
-btn.addEventListener('click', updateGridSquares);
-container.appendChild(btn);
-
+createButton();
 createGrid(16);
+
+function createButton() {
+    const btn = document.createElement('button');
+    btn.textContent = 'Update squares per side';
+    btn.addEventListener('click', updateGridSquares);
+    container.appendChild(btn);
+}
 
 function updateGridSquares() {
     squaresPerSide = prompt('Please enter the number of squares per side.');
@@ -27,37 +30,40 @@ function createGrid(squaresPerSide) {
     gridDiv.setAttribute('id', 'grid');
     gridDiv.style.display = 'flex';
     gridDiv.style.flexDirection = 'column';
-    gridDiv.style.gap = '10px;'
+
+    squareWidth = parseInt(GRID_WIDTH / squaresPerSide);
 
     for (let row = 0; row < squaresPerSide; row++) {
         const rowDiv = document.createElement('div');
         rowDiv.style.display = 'flex';
         rowDiv.style.justifyContent = 'center';
-        rowDiv.style.gap = '10px;';
+        rowDiv.style.maxWidth = GRID_WIDTH + 'px';
+        rowDiv.style.maxHeight = squareWidth + 'px';
         
         gridDiv.appendChild(rowDiv);
 
         for (let col = 0; col < squaresPerSide; col++) {
             const div = document.createElement('div');
-            div.style.height = '50px';
-            div.style.width = '50px';
+            div.setAttribute('id', 'square');
             div.style.border = 'solid 1px black'
-            div.style.textAlign = 'center'
+            div.style.width = squareWidth + 'px';
+            div.style.height = squareWidth + 'px';
+            div.style.boxSizing = 'border-box';
 
-
-            div.textContent = row + ', ' + col;
             rowDiv.appendChild(div);
         }
     }
 
     container.appendChild(gridDiv);
     gridDiv.addEventListener('mouseover', (event) => {
-        const target = event.target;
-        changeColor(target);
+        const id = event.target.id;
+        if (id === 'square') {
+            changeSquareColor(event.target);
+        }
     });
 }
 
-function changeColor(element) {
+function changeSquareColor(element) {
     element.style.backgroundColor = 'green';
     element.style.color = 'white';
 }
